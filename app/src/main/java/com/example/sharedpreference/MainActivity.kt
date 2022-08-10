1851cff03c148e5e3d7880a14d3c77bdaf765fe2
logonrmlocal <marinaldofsilva@gmail.com>
2022-08-09 22:27:57 -0300

    primeira parte

---
+++ app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.sharedpreference
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSalvar.setOnClickListener(View.OnClickListener {
+            val saudacaoPersistencia = this.getSharedPreferences( "saudacao", Context.MODE_PRIVATE)
+            val editor = saudacaoPersistencia.edit()
+
+            editor.putString("nome", txtNome.text.toString())
+            editor.apply()
+
+            Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show()
+
+
+        })
+
+        btnExibir.setOnClickListener(View.OnClickListener {
+
+
+
+            val intent = Intent(this, SaudacaoActivity :: class.java)
+            startActivity(intent)
+        })
+    }
+}
---
+++ app/src/main/java/com/example/sharedpreference/SaudacaoActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sharedpreference
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_saudacao.*
+
+class SaudacaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saudacao)
+
+        val saudacaoPeristencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+
+        val nome = saudacaoPeristencia.getString("nome", "")
+        val tratamento = saudacaoPeristencia.getString("tratamento", "")
+
+        if (tratamento.equals("Sem Tratamento")) {
+            lbSaudacao.text = nome
+        }else{
+            lbSaudacao.text = tratamento + "" + nome
+        }
+
+    }
+
+}
