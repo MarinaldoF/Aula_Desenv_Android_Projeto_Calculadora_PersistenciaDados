c3f194c86e40b162b6f69b77dd9cda531eb0085f
logonrmlocal <marinaldofsilva@gmail.com>
2022-08-09 22:40:26 -0300

    primeira parte teste ... teste2

--- app/src/main/java/com/example/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-
+//teste
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
