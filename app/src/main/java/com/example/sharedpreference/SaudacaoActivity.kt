18f26fa7175544982e1d659928b699cc89aa4c71
Marinaldo Ferreira <marinaldofsilva@gmail.com>
2022-08-10 21:38:57 -0300

    terceira parte

--- app/src/main/java/com/example/sharedpreference/SaudacaoActivity.kt
+++ app/src/main/java/com/example/sharedpreference/SaudacaoActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_saudacao.*
-
+//teste
 class SaudacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
