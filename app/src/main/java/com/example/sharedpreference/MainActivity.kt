e02fa773de2745d52e71eda49f53507acc817d8e
Marinaldo Ferreira <marinaldofsilva@gmail.com>
2022-08-14 22:30:31 -0300

    adicionado comentarios

--- app/src/main/java/com/example/sharedpreference/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -10,26 +10,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    //função que carrega aos configurações ao inicia a tela
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnSalvar.setOnClickListener(View.OnClickListener {
+            //carrega a biblioteca de preferences
             val saudacaoPersistencia = this.getSharedPreferences( "saudacao", Context.MODE_PRIVATE)
+            //pega o objeto responsavel pela edição dos dados da preferencias
             val editor = saudacaoPersistencia.edit()
 
             editor.putString("nome", txtNome.text.toString())
             editor.apply()
-
+            //exibe uma mensagem para o usuario que foi salvo com sucesso
             Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show()
 
 
         })
 
+        //exibe outra tela com outro layout
+
         btnExibir.setOnClickListener(View.OnClickListener {
 
 
-
+            //inicia a nova tela que o usuário irá navegar
             val intent = Intent(this, SaudacaoActivity :: class.java)
             startActivity(intent)
         })
--- app/src/main/java/com/example/sharedpreference/SaudacaoActivity.kt
+++ app/src/main/java/com/example/sharedpreference/SaudacaoActivity.kt
@@ -6,16 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_saudacao.*
 
 class SaudacaoActivity : AppCompatActivity() {
+    //função que carrega aos configurações ao inicia a tela
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudacao)
-
+        //pega os valores da persistencia
         val saudacaoPeristencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-
+        //pega a sttring da persistencia
         val nome = saudacaoPeristencia.getString("nome", "")
         val tratamento = saudacaoPeristencia.getString("tratamento", "")
-
+        //condição que verifica se foi feito um tipo de tratamento
         if (tratamento.equals("Sem Tratamento")) {
+
             lbSaudacao.text = nome
         }else{
             lbSaudacao.text = tratamento + "" + nome
